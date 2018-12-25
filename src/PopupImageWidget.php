240d50892d5e0d400d403f17f246cafc3be482ae
Robert <fatkhullin.robert@gmail.com>
2018-12-25 23:55:45 +0300

    Initial commit

---
+++ src/PopupImageWidget.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace b4djo\popup_image;
+
+use yii\base\Widget;
+
+/**
+ * Class PopupImageWidget
+ * @package b4djo\popup_image
+ */
+class PopupImageWidget extends Widget
+{
+    /**
+     * @var string
+     */
+    public $thumbnail;
+
+    /**
+     * @var string
+     */
+    public $source;
+
+    /**
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->registerAssets();
+    }
+
+    /**
+     * @return string
+     */
+    public function run()
+    {
+        return $this->render('view');
+    }
+
+    /**
+     * Register assets
+     */
+    private function registerAssets()
+    {
+        $path = \Yii::$app->getAssetManager()->publish(__DIR__ . '/assets/');
+        $this->getView()->registerCssFile($path[1] . '/css/lightbox.css');
+        $this->getView()->registerJsFile(
+            $path[1] . '/js/lightbox.js',
+            [
+                'position' => \yii\web\View::POS_END,
+                'depends'  => ['\yii\web\JqueryAsset'],
+            ]
+        );
+    }
+}
---
+++ src/views/view.php
@@ -0,0 +1,9 @@
+<?php
+
+$widget = $this->context;
+
+?>
+
+<a data-lightbox="image-1" href="<?= $widget->source ?>">
+    <img src="<?= $widget->thumbnail ?>" height="115px" width="150px" alt="<?= $widget->description ?>" />
+</a>
